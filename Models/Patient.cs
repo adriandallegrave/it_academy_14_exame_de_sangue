d332ce2e73ec9d05c1f3d1955b7556c2a398a2ba
arthur <arthur.g.dsant@gmail.com>
2021-12-08 10:44:54 -0300

    feat: add folder models and request file

---
+++ Models/Patient.cs
@@ -0,0 +1,7 @@
+namespace BloodCheck.Models;
+
+public class Patient{
+    public string? name {get; set;}
+    public char[] cpf {get; set;}
+    public char[] phone {get; set;}
+}    
---
+++ Models/Request.cs
@@ -0,0 +1,3 @@
+namespace BloodCheck.Models;
+
+    
