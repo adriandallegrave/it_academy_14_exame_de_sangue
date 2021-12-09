3f571c47b4af5cca5628eda93461f20ba6413743
joaogabrieldlb <joao.bergamaschi@edu.pucrs.br>
2021-12-09 18:48:04 -0300

    fix: rename properties to pascal case

--- BloodCheck/Models/Patient.cs
+++ BloodCheck/Models/Patient.cs
@@ -2,10 +2,10 @@ namespace BloodCheck.Models;
 
 public class Patient
 {
-    public int patientId {get; set;}
-    public string? name {get; set;}
-    public string? cpf {get; set;}
-    public string? phone {get; set;}
+    public int PatientId { get; set; }
+    public string? Name { get; set; }
+    public string? Cpf { get; set; }
+    public string? Phone { get; set; }
 
     // List to generate foreign key (Patient 1 to many Requests).
     public List<Request> Requests { get; set; } = null!;
--- BloodCheck/Models/RequestExam.cs
+++ BloodCheck/Models/RequestExam.cs
@@ -5,10 +5,10 @@ public class RequestExam
     // Class to generate composite Primary key.
     
     // Create foreign key Exam (RequestExam many to 1 Exam).
-    public int examId { get; set; }
+    public int ExamId { get; set; }
     public Exam Exam { get; set; } = null!;
 
     // Create foreign key Request (RequestExam many to 1 Request).
-    public int requestId { get; set; }
+    public int RequestId { get; set; }
     public Request Request { get; set; } = null!;
 }
