5a026656137c9a96c9e87a01993d841d1986219c
LuizFelipePLO <felipe08074@gmail.com>
2021-12-09 17:03:12 -0300

    Update new_requisition.dart
    bottom app bar creation (to be fixed)

--- blood_check/lib/screens/new_requisition.dart
+++ blood_check/lib/screens/new_requisition.dart
@@ -1,3 +1,4 @@
+import 'package:blood_check/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -15,17 +16,51 @@ class _NewRequisitionState extends State<NewRequisition> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[600],
-          centerTitle: true,
-          title: const Text('Blood Check',
-              style: TextStyle(fontSize: 24, color: Colors.white)),
-        ),
-        body: Container(
-            //padding: const EdgeInsets.all(40),
-            child: Wrap(spacing: 8.0, runSpacing: 4.0, children: <Widget>[
-          PatientData(),
-          RequisitionData(),
-        ]))); // Scaffold
+      appBar: AppBar(
+        backgroundColor: Colors.blue[600],
+        centerTitle: true,
+        title: const Text('Blood Check',
+            style: TextStyle(fontSize: 24, color: Colors.white)),
+      ),
+      body: Container(
+          //padding: const EdgeInsets.all(40),
+          child: Wrap(spacing: 8.0, runSpacing: 4.0, children: <Widget>[
+        PatientData(),
+        RequisitionData(),
+      ])),
+      bottomNavigationBar: BottomAppBar(
+        color: kPrimaryColor,
+        child: Column(children: [
+          Row(children: [
+            //Spacer(),
+            RichText(
+              text: TextSpan(
+                  text: 'Prazo de Entrega Final:',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold)),
+            )
+          ]),
+          Row(
+            children: [
+              RichText(
+                text: TextSpan(
+                    text: 'Total:',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold)),
+              )
+            ],
+          ),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: kPrimaryColor,
+          child: Icon(Icons.check_circle_outline),
+          onPressed: () {}),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    ); // Scaffold
   }
 }
